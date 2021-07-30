2059900bc7fe59ffadb834280db2e1c957e450d0
o-code-cyber <demirorhan0555@gmail.com>
2021-07-30 15:39:50 +0300

    first commit

---
+++ src/FirstProjPacakge/FirstJavaClass.java
@@ -0,0 +1,11 @@
+package FirstProjPacakge;
+
+public class FirstJavaClass {
+	public static void main(String[] args) {
+		System.out.println("Hello Java and Selenium");
+		for(int i = 0; i<100;i++){ 
+		System.out.println("Selenium");
+		}
+	}
+
+}
